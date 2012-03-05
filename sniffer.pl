5e64ca89f4f169c7cb646773883458d0d7f155fd
spa <spa@alpha.ru>
2012-03-05 16:07:30 +0400

    XML YAML strictires added printing only by important headers differential by all important headers

--- sniffer.pl
+++ sniffer.pl
@@ -1,23 +1,30 @@
 #! /usr/bin/perl -w
+# вызывает warning о неинициализированное переменной
+# "," в конце вывода Perl структуры
 use strict;
 use Net::Pcap;
   use NetPacket::Ethernet qw(:strip);
   use NetPacket::IP qw(:strip);
   use NetPacket::TCP;
+#use XML::Simple;
+
 #хеш ссылок на массив значений заголовков, по названиям
 my @user_base; 
+my $structure_type = "p";
 
 my $default_filter = "tcp and port 80 ";
 my @headers = ("Age", "TE", "URI", "Via");
-my @is_important_headers = (
-	"User-Agent", 
+my @important_headers = (
+	"User-Agent",
 	"Accept", 
 	"Accept-Language", 
 	"Accept-Encoding",
 	"Accept-Charset",
-	"Refer"	);
+	"Refer",
+	 
+	);
 
-my $count = 10;
+my $count = 0x7fffffff;
 my @args;
 
 while (defined ($_ = shift @ARGV)){
@@ -25,8 +32,16 @@ while (defined ($_ = shift @ARGV)){
 		$count = shift @ARGV;
 	}
 	elsif($_ eq "-h"){
-		print "sniffer.pl [ -c count ] \n[ - i interface ]  [ BPF ]\nOR\n[ -f file] [ BPF ]\n";
-
+		warn "sniffer.pl [ -c count ] [ -pxy ]\n[ - i interface ]  [ BPF ]\nOR\n[ -f file] [ BPF ]\n";
+	}
+	elsif($_ eq "-p"){
+		$structure_type = "p";
+	}
+	elsif($_ eq "-x"){
+		$structure_type = "x";
+	}
+	elsif($_ eq "-y"){
+		$structure_type = "y";
 	}
 	else{
 		push (@args,$_);
@@ -35,18 +50,17 @@ while (defined ($_ = shift @ARGV)){
 @ARGV = @args;
 
 if(defined($_ = $ARGV[0])){
-	print "getting $count packets\n";
+	
 
 	if ($_ eq "-f"){
 		my $err = '';
 		my $net;
 		my $mask;
 		my $filter;
-
 		my ($_, $dump, @filter_list) = @ARGV;
-
 		my $filter_str = &build_filter(@filter_list);
-		print "from file:\t$dump\nwith BPF:\t$filter_str\n";
+
+		&greeting($count,"f",$dump,$filter_str);
 
 		my $pcap = Net::Pcap::pcap_open_offline($dump, \$err)
       		  or die "Can't read '$dump': $err\n";
@@ -67,7 +81,9 @@ if(defined($_ = $ARGV[0])){
 
 		my ($_, $dev, @filter_list) = @ARGV;
 		my $filter_str = &build_filter(@filter_list);
-		print "from interface:\t$dev\nwith BPF:\t$filter_str\n";
+
+		&greeting($count,"i",$dev,$filter_str);
+
 
 		my $pcap = Net::Pcap::pcap_open_live($dev, 1024, 1, 0, \$err)
 			or die "Can't open device $dev: $err\n";
@@ -92,10 +108,8 @@ if(defined($_ = $ARGV[0])){
 
 	exit (0);
 }
-print "sniffer.pl [ -c count ] \n[ - i interface ]  [ BPF ]\nOR\n[ -f file] [ BPF ]\n";
+warn "sniffer.pl [ -c count ] [ -pxy ]\n[ - i interface ]  [ BPF ]\nOR\n[ -f file] [ BPF ]\n";
 exit (-1);
-
-
 
 sub build_filter{
 	my (@filter_list) = @_;
@@ -138,11 +152,11 @@ sub push_http_pack {	# get inf from packet and push it in base
 	my $oldref;
 	my $Request_Header_flag = 0;
 	foreach (@packet_by_str){
-		if(m/^\h*([!-~]*?):\h*(.+)\h*$/s){#m/\h*([A-Z]\w{2,}):\h*(\w{2,}.+)\h*/s
+		if(m/^\h*([!-~]*?):\h*(.+)\h*$/s){
 			#make a new hash from imporrtant headers by this packet
 			if (&is_important($1) ){
 				${$newref}{$1} = $2;
-				$Request_Header_flag = 1	if($1 eq "User-Agent"); 
+				$Request_Header_flag = 1; 
 			}
 			else{
 				#warn "NOTIMPHEADER(or is ib base):\t$1\nIN:$_\n";
@@ -161,11 +175,6 @@ sub push_http_pack {	# get inf from packet and push it in base
 	if($Request_Header_flag){
 			foreach $oldref (@user_base){
 			if (&user_eq($newref, $oldref)){ #is it old user?
-				foreach my $temp(@is_important_headers){	#adding new headers for this user
-					unless(defined ($$oldref{$temp})){
-						$$oldref{$temp} = $$newref{$temp} if (defined($$newref{$temp}));
-					}			
-				}
 				return 0;
 			}
 		}
@@ -173,17 +182,46 @@ sub push_http_pack {	# get inf from packet and push it in base
 	}
 }
 sub print_base{
-	my $key;
-	my $value;
-	my $i = 0;
+	if($structure_type eq "p"){
+		my $i = 0;
+		my $temp;
+		foreach (@user_base){
+			print "my \$user_$i = (\n";
+				foreach $temp (@important_headers){
+					print "\t\'$temp\' => \'$$_{$temp}\',\n"; #warning if it unitialized
+				}
+			print ")\n\n";
+			++$i;
+		}
+	}
+	elsif($structure_type eq "x"){
+		use XML::Simple;
+		my %output_hash;	
+		my $username;
+		my $i = 0;
+		foreach (@user_base){
+			$username = "user_$i";
+			$i++;
+			$output_hash{$username} = $_;
+		}
 
-	foreach (@user_base){
-		print "my user_$i = (\n";
-			while(($key, $value) = each %$_ ){
-				print "\t\'$key\' => \'$value\',\n";
-			}
-		print ")\n\n";
-		++$i;
+		my $simple = XML::Simple->new();
+		my $out = $simple->XMLout(\%output_hash);
+		print "$out\n";
+	}
+	elsif($structure_type eq "y"){
+		use YAML;
+
+		my %output_hash;
+		my $username;
+		my $i = 0;
+		foreach (@user_base){
+			$username = "user_$i";
+			$i++;
+			$output_hash{$username} = $_;
+		}
+		my $out = YAML::Dump(%output_hash);
+		print "$out";
 	}
 }
 sub attend_str_in_array{
@@ -197,16 +235,35 @@ sub attend_str_in_array{
 }
 sub is_important{
 	my ($arg) = @_;
-	return &attend_str_in_array($arg, @is_important_headers);
-	#	return (
-	#		 (	("$arg" =~ m/^[A-Z]\w.*$/) ||
-	#		 	(&attend_str_in_array($arg, @headers))
-	#		 )&&
-	#		 !("$arg" =~ m/^cookie/i)
-	#	);
+	return &attend_str_in_array($arg, @important_headers);
 }
 sub user_eq{
 	my ($r1, $r2) = @_; 
-	return (	defined($$r1{"User-Agent"}) &&
-		$$r1{"User-Agent"} eq  $$r2{"User-Agent"});
+	foreach (@important_headers){
+		if ($$r1{$_} ne $$r2{$_}){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+sub greeting(){
+	my ($count,$flag, $dev, $filter_str) = @_;
+	warn "getting $count packets\n";
+	if($flag eq "f"){
+		warn "from file:\t$dev\n"
+	}
+	elsif($flag eq "i"){
+		warn "from interface:\t$dev\n"
+	}
+	warn "with BPF:\t$filter_str\n";
+	if($structure_type eq "p"){
+		warn "print as Perl structure.\n";
+	}
+	elsif($structure_type eq "x"){
+		warn "print as XML structure.\n";
+	}
+	elsif($structure_type eq "y"){
+		warn "print as YAML structure.\n";
+	}
 }
